eebab579a7c6dd17d0f7f7275d48b7beaf182a06
singhplusplus <79360839+singhplusplus@users.noreply.github.com>
2021-06-02 16:02:54 +0530

    Add files via upload

---
+++ src/app/unauthorized/unauthorized.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { UnauthorizedComponent } from './unauthorized.component';
+
+describe('Component: Unauthorized', () => {
+  it('should create an instance', () => {
+    let component = new UnauthorizedComponent();
+    expect(component).toBeTruthy();
+  });
+});
